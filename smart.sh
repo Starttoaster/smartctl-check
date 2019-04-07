4b0be720dcecdaef1bc0769a2c87eab818fa7b2a
Brandon Butler <bmbawb@gmail.com>
2019-04-07 18:11:00 -0500

    Removed extraneous character '*'
    Turns out the '*' was unnecessary and did have any bearing on the output; as such it was dropped.

--- smart.sh
+++ smart.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 #Starts self-test
-ls /dev/sd* | sed 's/[0-9]*//g' | sort -u | while read -r line; do
+ls /dev/sd* | sed 's/[0-9]//g' | sort -u | while read -r line; do
         sudo smartctl -t short $line | grep "successful";done
 sleep 125s
 
 #Shows S.M.A.R.T. attributes and test results.
-ls /dev/sd* | sed 's/[0-9]*//g' | sort -u | while read -r line; do
+ls /dev/sd* | sed 's/[0-9]//g' | sort -u | while read -r line; do
         echo ============= $line =============
         sudo smartctl -a $line | grep "Raw_Read\|Power_On\|Power_Cycle\|Reallocated\|Timeout\|Media_Wearout\|CurrentPending\|Uncorrectable"
         sudo smartctl -l selftest $line | grep "# 1";done

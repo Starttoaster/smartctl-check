88397d54b42baba0eeb302f464b65900c4305bcb
Brandon Butler <bmbawb@gmail.com>
2019-04-08 04:32:27 -0500

    Making separate script to run tests.

--- smart.sh
+++ smart.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-#Starts self-test
-ls /dev/sd* | sed 's/[0-9]//g' | sort -u | while read -r line; do
-        sudo smartctl -t short $line | grep "successful";done
-sleep 125s
-
 #Shows S.M.A.R.T. attributes and test results.
 ls /dev/sd* | sed 's/[0-9]//g' | sort -u | while read -r line; do
         echo ============= $line =============

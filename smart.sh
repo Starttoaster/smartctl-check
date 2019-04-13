4a77396221f11ab80c6789ebd16c10757a448b23
Brandon Butler <bmbawb@gmail.com>
2019-04-13 08:28:23 -0500

    Fixed typo

--- smart.sh
+++ smart.sh
@@ -3,4 +3,4 @@
 #Shows S.M.A.R.T. attributes and test results.
 ls /dev/sd* | sed 's/[0-9]//g' | sort -u | while read -r line; do
         echo -e "\n============= $line ============="
-        smartctl -a /dev/sda | grep "ID#\|Raw_Read\|Power_On\|Power_Cycle\|Reallocated\|Timeout\|Media_Wearout\|CurrentPending\|Uncorrectable\|Test_Description\|# 1";done
+        smartctl -a $line | grep "ID#\|Raw_Read\|Power_On\|Power_Cycle\|Reallocated\|Timeout\|Media_Wearout\|CurrentPending\|Uncorrectable\|Test_Description\|# 1";done
